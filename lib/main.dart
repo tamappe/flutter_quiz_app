59ace4c6d206caf1fb5c22ed674f4dec8db98994
tamaoki <treasure6hunter9cole@gmail.com>
2020-02-22 19:14:37 +0900

    private クラス、メソッド、プロパティの導入

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,19 @@ void main() => runApp(MyApp());
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return new MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
+class _MyAppState extends State<MyApp> {
 
-  int questionIndex = 0;
+  int _questionIndex = 0;
 
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
-      questionIndex = questionIndex + 1;
+      _questionIndex = _questionIndex + 1;
     });
-    print(questionIndex);
+    print(_questionIndex);
   }
 
   @override
@@ -33,19 +33,19 @@ class MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Text(questions[questionIndex]),
+            Text(questions[_questionIndex]),
             RaisedButton(
                 child: Text('Answer 1'),
-                onPressed: answerQuestion
+                onPressed: _answerQuestion
             ),
             RaisedButton(
                 child: Text('Answer 1'),
-                onPressed: answerQuestion
+                onPressed: _answerQuestion
             ),
             RaisedButton(
                 child: Text('Answer 1'),
                 onPressed: () {
-                  answerQuestion();
+                  _answerQuestion();
                 }
             ),
           ],
