e19262bae31cfe3ed56ddb7325425c41d30e09a8
tamaoki <treasure6hunter9cole@gmail.com>
2020-02-22 19:09:18 +0900

    StatefulWidget の導入

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,54 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+
+  int questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s is your favorite color?',
+      'Waht\'s is your favorite animal?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my default text!'),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            RaisedButton(
+                child: Text('Answer 1'),
+                onPressed: answerQuestion
+            ),
+            RaisedButton(
+                child: Text('Answer 1'),
+                onPressed: answerQuestion
+            ),
+            RaisedButton(
+                child: Text('Answer 1'),
+                onPressed: () {
+                  answerQuestion();
+                }
+            ),
+          ],
+        ),
       ),
     );
   }
